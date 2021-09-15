bda9d974e8763075af735d6bbedc243c7906aa58
Hunter Wilhelm <zicy@zicy.net>
2021-09-15 13:46:29 -0600

    week 01 team

--- week01/team/team.py
+++ week01/team/team.py
@@ -44,26 +44,40 @@ def is_prime(n: int) -> bool:
         i += 6
     return True
 
-
-if __name__ == '__main__':
-    log = Log(show_terminal=True)
-    log.start_timer()
-
+def run_threading(start: int, end: int, step: int):
+    global prime_count
     # TODO 1) Get this program running
     # TODO 2) move the following for loop into 1 thread
     # TODO 3) change the program to divide the for loop into 10 threads
-
-    start = 10000000000
-    range_count = 100000
-    for i in range(start, start + range_count):
+    for i in range(start, end, step):
         if is_prime(i):
             prime_count += 1
             print(i, end=', ', flush=True)
     print(flush=True)
 
+
+
+if __name__ == '__main__':
+    log = Log(show_terminal=True)
+    log.start_timer()
+
+    num_threads = 10
+    threads = []
+    start = 10000000000
+    range_count = 100000
+    end = start + range_count
+
+    for i in range(num_threads):
+        t = threading.Thread(target=run_threading, args=(start + i, end, num_threads))
+        threads.append(t)
+    
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+    
     # Should find 4306 primes
     log.write(f'Numbers processed = {numbers_processed}')
     log.write(f'Primes found      = {prime_count}')
     log.stop_timer('Total time')
-
-
