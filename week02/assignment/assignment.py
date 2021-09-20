1bb4fc0ec9b15a78ff249d56f1b443aa2ef004d6
Hunter Wilhelm <zicy@zicy.net>
2021-09-20 12:11:23 -0600

    w02 assignment

--- week02/assignment/assignment.py
+++ week02/assignment/assignment.py
@@ -39,26 +39,101 @@ from cse251 import *
 
 # Const Values
 TOP_API_URL = r'https://swapi.dev/api'
+TYPES = ["characters", "planets", "starships", "vehicles", "species"]
 
 # Global Variables
 call_count = 0
 
 
 # TODO Add your threaded class definition here
+class RetrieveJson(threading.Thread):
+    def __init__(self, url, type=None):
+        super().__init__()
+        self.url = url
+        self.type = type
+    
+    def run(self):
+        try:
+            self.results = self._retrieve_json(self.url)
+        except AssertionError:
+            self.results = None
+    
+    def _retrieve_json(self, url):
+        global call_count
+        call_count += 1
+
+        response = requests.get(url)
+        if response.status_code != 200:
+            print('Error in requesting url:', url)
+            print('Expected status code 200. Got:', response.status_code)
+            raise AssertionError
+        return response.json()
 
 
 # TODO Add any functions you need here
 
+
+def retrieve_top_api_urls(url):
+    thread = RetrieveJson(url)
+    run_threads((thread,))
+    return thread.results
+
+def retrieve_details_on_film(urls, film_id):
+    url = urls["films"] + str(film_id)
+    thread = RetrieveJson(url)
+    run_threads((thread,))
+    return thread.results
+
+def make_threads(details):
+    threads = []
+    for type in TYPES:
+        for url in details[type]:
+            thread = RetrieveJson(url, type)
+            threads.append(thread)
+    return threads
+
+def run_threads(threads):
+    for t in threads:
+        t.start()
+    
+    for t in threads:
+        t.join()
+
+def retrieve_data_from_threads(threads):
+    data = {t: [] for t in TYPES}
+    for t in threads:
+        data[t.type].append(t.results["name"])
+    for t in TYPES:
+        data[t] = sorted(data[t])
+    return data
+
+def display_data(log, details, data):
+    log.write("----------------------------------------")
+    log.write("Title   : " + details["title"])
+    log.write("Director: " + details["director"])
+    log.write("Producer: " + details["producer"])
+    log.write("Released: " + details["release_date"])
+    log.write()
+    for t in TYPES:
+        log.write(t.capitalize() + ": " + str(len(data[t])))
+        log.write(", ".join(data[t]) + ",")
+        log.write()
 
 def main():
     log = Log(show_terminal=True)
     log.start_timer('Starting to retrieve data from swapi.dev')
 
     # TODO Retrieve Top API urls
+    urls = retrieve_top_api_urls(TOP_API_URL)
 
-    # TODO Retireve Details on film 6
-
+    # TODO Retrieve Details on film 6
+    details = retrieve_details_on_film(urls, 6)
+    threads = make_threads(details)
+    run_threads(threads)
+    data = retrieve_data_from_threads(threads)
+    
     # TODO Display results
+    display_data(log, details, data)
 
     log.stop_timer('Total Time To complete')
     log.write(f'There were {call_count} calls to swapi server')
