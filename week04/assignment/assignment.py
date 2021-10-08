6c3cd878d567203a517c59b08131e50900f1cdd6
Hunter Wilhelm <zicy@zicy.net>
2021-10-08 13:38:03 -0600

    done

--- week04/assignment/assignment.py
+++ week04/assignment/assignment.py
@@ -77,32 +77,53 @@ class Queue251():
 class Factory(threading.Thread):
     """ This is a factory.  It will create cars and place them on the car queue """
 
-    def __init__(self):
+    def __init__(self,
+                 shared_queue: Queue251,
+                 factory_spots: threading.Semaphore,
+                 dealer_spots: threading.Semaphore):
         # TODO, you need to add arguments that will pass all of data that 1 factory needs
         # to create cars and to place them in a queue.
-        pass
+        self.shared_queue = shared_queue
+        self.factory_spots = factory_spots
+        self.dealer_spots = dealer_spots
+        super().__init__()
 
 
     def run(self):
-        for i in range(CARS_TO_PRODUCE):
+        for _ in range(CARS_TO_PRODUCE):
             # TODO Add you code here
             """
             create a car
             place the car on the queue
             signal the dealer that there is a car on the queue
-           """
+            """
+            self.factory_spots.acquire()
+            car = Car()
+            self.shared_queue.put(car)
+            self.dealer_spots.release()
 
         # signal the dealer that there there are not more cars
-        pass
+        self.factory_spots.acquire()
+        self.shared_queue.put(None)
+        self.dealer_spots.release()
+
 
 
 class Dealer(threading.Thread):
     """ This is a dealer that receives cars """
 
-    def __init__(self):
-        # TODO, you need to add arguments that pass all of data that 1 factory needs
-        # to create cars and to place them in a queue
-        pass
+    def __init__(self,
+                 shared_queue: Queue251,
+                 factory_spots: threading.Semaphore, 
+                 dealership_spots: threading.Semaphore, 
+                 queue_stats: threading.Semaphore):
+        # TODO, you need to add arguments that pass all of data that 1 dealer needs
+        # to sell cars and signal the factory that there is an empty slot in the queue
+        self.shared_queue = shared_queue
+        self.factory_spots = factory_spots
+        self.dealership_spots = dealership_spots
+        self.queue_stats = queue_stats
+        super().__init__()
 
     def run(self):
         while True:
@@ -111,10 +132,21 @@ class Dealer(threading.Thread):
             take the car from the queue
             signal the factory that there is an empty slot in the queue
             """
+            self.dealership_spots.acquire()
+            stats_size_index = self.shared_queue.size() - 1
+            # If we had more than 1 dealer, then we would need a lock here
+            self.queue_stats[stats_size_index] += 1
+
+            car = self.shared_queue.get()
+            if car is None:
+                # if there are other threads, let them know too
+                self.shared_queue.put(None)
+                break
+            self.factory_spots.release()
 
             # Sleep a little after selling a car
             # Last statement in this for loop - don't change
-            time.sleep(random.random() / (SLEEP_REDUCE_FACTOR))
+            time.sleep(random.random() / (SLEEP_REDUCE_FACTOR) *)
 
 
 
@@ -124,20 +156,30 @@ def main():
     # TODO Create semaphore(s)
     # TODO Create queue251 
     # TODO Create lock(s) ?
+    factory_spots = threading.Semaphore(MAX_QUEUE_SIZE)
+    dealership_spots = threading.Semaphore(0)
+    shared_queue = Queue251()
+    # If we had more than 1 dealer, then we would need a lock here
 
     # This tracks the length of the car queue during receiving cars by the dealership
     # i.e., update this list each time the dealer receives a car
     queue_stats = [0] * MAX_QUEUE_SIZE
 
     # TODO create your one factory
+    factory = Factory(shared_queue, factory_spots, dealership_spots)
 
     # TODO create your one dealership
+    dealership = Dealer(shared_queue, factory_spots, dealership_spots, queue_stats)
 
     log.start_timer()
 
     # TODO Start factory and dealership
+    factory.start()
+    dealership.start()
 
     # TODO Wait for factory and dealership to complete
+    factory.join()
+    dealership.join()
 
     log.stop_timer(f'All {sum(queue_stats)} have been created')
 
