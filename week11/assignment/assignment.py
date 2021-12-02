586dca4c42f0d565048d17f1a30de49ae48787db
Hunter Wilhelm <zicy@zicy.net>
2021-12-01 18:57:58 -0700

    Week 11 prove and week 12 team 1

--- week11/assignment/assignment.py
+++ week11/assignment/assignment.py
@@ -4,6 +4,8 @@ Lesson Week: 11
 File: Assignment.py
 """
 
+from multiprocessing import process
+from multiprocessing.context import Process
 import time
 import random
 import multiprocessing as mp
@@ -40,7 +42,7 @@ def guest_partying(id):
     time.sleep(random.uniform(0, 1))
 
 # -----------------------------------------------------------------------------
-def cleaner():
+def cleaner(pid, end_time, clean_lock, cleaned_count):
     """
     do the following for TIME seconds
     cleaner will wait to try to clean the room (cleaner_waiting())
@@ -49,10 +51,25 @@ def cleaner():
         Take some time cleaning (cleaner_cleaning())
         display message STOPPING_CLEANING_MESSAGE
     """
-    pass
+    while end_time > time.time():
+        
+        # wait
+        cleaner_waiting()
+
+        # get access
+        with clean_lock:
+            print(STARTING_CLEANING_MESSAGE)
+
+            # do cleaning etc.
+            cleaner_cleaning(pid)
+            print(STOPPING_CLEANING_MESSAGE)
+
+        # count parties
+        with cleaned_count.get_lock():
+            cleaned_count.value += 1
 
 # -----------------------------------------------------------------------------
-def guest():
+def guest(pid, end_time, clean_lock, party_count, party_lock, waiting_count):
     """
     do the following for TIME seconds
     guest will wait to try to get access to the room (guest_waiting())
@@ -61,18 +78,67 @@ def guest():
         Take some time partying (guest_partying())
         display message STOPPING_PARTY_MESSAGE if the guest is the last one leaving in the room
     """
-    pass
+    while end_time > time.time():
+        guest_waiting()
+
+        # get access to the room and assign who turns on the light
+        with waiting_count.get_lock():
+            light_on_duty = waiting_count.value == 0
+            waiting_count.value += 1
+        
+        # turn on the lights
+        # one person fits through the door at a time
+        # avoids polling
+        with party_lock:
+            if light_on_duty:
+                clean_lock.acquire()
+                print(STARTING_PARTY_MESSAGE)
+
+        # party hard
+        guest_partying(pid)
+
+        # leave the party and assign who turns off the light
+        with waiting_count.get_lock():
+            waiting_count.value -= 1
+            light_off_duty = waiting_count.value == 0
+        
+        # turn off the lights
+        # let the cleaners in
+        if light_off_duty:
+            print(STOPPING_PARTY_MESSAGE)
+            clean_lock.release()
+            
+            # count parties
+            with party_count.get_lock():
+                party_count.value += 1
 
 # -----------------------------------------------------------------------------
 def main():
     # TODO - add any variables, data structures, processes you need
-    # TODO - add any arguments to cleaner() and geust() that you need
+    waiting_count = mp.Value("i", 0)
+    cleaned_count = mp.Value("i", 0)
+    party_count = mp.Value("i", 0)
+    clean_lock = mp.Lock()
+    party_lock = mp.Lock()
+    people: list[Process] = []
 
     # Start time of the running of the program. 
     start_time = time.time()
+    end_time = start_time + TIME
+
+    # TODO - add any arguments to cleaner() and guest() that you need
+    people.extend([mp.Process(target=cleaner, args=(i+1, end_time, clean_lock, cleaned_count)) for i in range(CLEANING_STAFF)])
+    people.extend([mp.Process(target=guest,   args=(i+1, end_time, clean_lock, party_count, party_lock, waiting_count))   for i in range(HOTEL_GUESTS)])
+
+    for p in people:
+        p.start()
+    
+    for p in people:
+        p.join()
+    
 
     # Results
-    print(f'Room was cleaned {cleaned_count} times, there were {party_count} parties')
+    print(f'Room was cleaned {cleaned_count.value} times, there were {party_count.value} parties')
 
 
 if __name__ == '__main__':
--- week12/team1/team1.go
+++ week12/team1/team1.go
@@ -45,6 +45,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -67,7 +68,47 @@ type Person struct {
 	Vehicles   []string
 }
 
-func getPerson(url string) {
+type Species struct {
+	Name           string
+	Classification string
+	Average_height string
+	Language       string
+}
+
+func getSpecies(wg *sync.WaitGroup, url string) {
+	// make a sample HTTP GET request
+	res, err := http.Get(url)
+
+	// check for response error
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// read all response body
+	data, _ := ioutil.ReadAll(res.Body)
+
+	// close response body
+	res.Body.Close()
+
+	// fmt.Println(string(data))
+
+	species := Species{}
+	jsonErr := json.Unmarshal(data, &species)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+		fmt.Println("ERROR Pasing the JSON")
+	}
+
+	fmt.Println("-----------------------------------------------")
+	// fmt.Println(person)
+	fmt.Println("Name      			: ", species.Name)
+	fmt.Println("Average height     : ", species.Average_height)
+	fmt.Println("Language 			: ", species.Language)
+
+	wg.Done()
+}
+
+func getPerson(wg *sync.WaitGroup, url string) {
 	// make a sample HTTP GET request
 	res, err := http.Get(url)
 
@@ -96,6 +137,8 @@ func getPerson(url string) {
 	fmt.Println("Name      : ", person.Name)
 	fmt.Println("Birth     : ", person.Birth_year)
 	fmt.Println("Eye color : ", person.Eye_color)
+
+	wg.Done()
 }
 
 func main() {
@@ -120,9 +163,41 @@ func main() {
 		"http://swapi.dev/api/people/81/",
 	}
 
+	var wg sync.WaitGroup
+
 	for _, url := range urls {
-		getPerson(url)
+		go getPerson(&wg, url)
+		wg.Add(1)
+	}
+	wg.Wait()
+
+	urls2 := []string{
+		"http://swapi.dev/api/species/1/",
+		"http://swapi.dev/api/species/2/",
+		"http://swapi.dev/api/species/3/",
+		"http://swapi.dev/api/species/6/",
+		"http://swapi.dev/api/species/15/",
+		"http://swapi.dev/api/species/19/",
+		"http://swapi.dev/api/species/20/",
+		"http://swapi.dev/api/species/23/",
+		"http://swapi.dev/api/species/24/",
+		"http://swapi.dev/api/species/25/",
+		"http://swapi.dev/api/species/26/",
+		"http://swapi.dev/api/species/27/",
+		"http://swapi.dev/api/species/28/",
+		"http://swapi.dev/api/species/29/",
+		"http://swapi.dev/api/species/30/",
+		"http://swapi.dev/api/species/33/",
+		"http://swapi.dev/api/species/34/",
+		"http://swapi.dev/api/species/35/",
+		"http://swapi.dev/api/species/36/",
+		"http://swapi.dev/api/species/37/",
 	}
 
+	for _, url := range urls2 {
+		go getSpecies(&wg, url)
+		wg.Add(1)
+	}
+	wg.Wait()
 	fmt.Println("All done!")
 }
